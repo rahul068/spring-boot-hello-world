c52f3873ddc1080fcc9d1d5294023cd72ab6b8a9
rahul068 <rahul.jaiswal068@gmail.com>
2020-05-26 10:26:30 +0530

    Testing

--- src/main/java/com/patrickgrimard/Application.java
+++ src/main/java/com/patrickgrimard/Application.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
 @EnableAutoConfiguration
 public class Application {
 
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
